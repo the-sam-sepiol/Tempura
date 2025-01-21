6e4f2ca52e18a658432e10f3cf6860a65bc58b29
Ethan <ela22d@fsu.edu>
2025-01-21 15:42:41 -0500

    fixed more

---
+++ client/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/index.css'; // Import global styles (includes Tailwind CSS)
+
+// Find the root element in the index.html file
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
