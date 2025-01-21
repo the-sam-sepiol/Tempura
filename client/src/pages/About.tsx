9a56cea314aec77813c48bf347af1475eb815107
Ethan <ela22d@fsu.edu>
2025-01-21 16:03:29 -0500

    final commits for this branch

---
+++ client/src/pages/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div className="text-center p-4">
+      <h1 className="text-2xl font-bold">About Page</h1>
+      <p>This is the About Page!</p>
+    </div>
+  );
+};
+
+export default About;
