90e8478a95f40879ae4362dd8995ac028de94873
the-sam-sepiol <152436775+the-sam-sepiol@users.noreply.github.com>
2025-03-26 22:52:09 -0400

    Rework routing structure, remove header navbar from login and signup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header.tsx';
 import Login from './pages/login.tsx';
 import Home from './pages/Home.tsx';
 import SignUp from './pages/signup.tsx'; 
 import Decision from './pages/decision.tsx';
 import Account from './pages/Account.tsx';
 import About from './pages/About.tsx';
+import MainLayout from './components/MainLayout.tsx';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Header />
       <Routes>
-        <Route path="/" element={<Decision />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/account" element ={<Account />} />
-        <Route path="/about" element = {<About />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Decision />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/about" element={<About />} />
+        </Route>
       </Routes>
     </Router>
   );
