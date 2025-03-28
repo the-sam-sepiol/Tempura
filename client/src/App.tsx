51fe4dd1516db4455cedd65a0a19f8f618cc78f7
the-sam-sepiol <152436775+the-sam-sepiol@users.noreply.github.com>
2025-03-27 21:20:37 -0400

    Accidental Delete

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/login.tsx";
-import Home from "./pages/Home.tsx";
-import SignUp from "./pages/signup.tsx";
-import Decision from "./pages/decision.tsx";
-import Account from "./pages/Account.tsx";
-import About from "./pages/About.tsx";
-import Friends from "./pages/Friends.tsx";
-import MainLayout from "./components/MainLayout.tsx";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/login.tsx';
+import Home from './pages/Home.tsx';
+import SignUp from './pages/signup.tsx'; 
+import Decision from './pages/decision.tsx';
+import Account from './pages/Account.tsx';
+import About from './pages/About.tsx';
+import MainLayout from './components/MainLayout.tsx';
+import Friends from './pages/Friends.tsx';
 
 const App: React.FC = () => {
   return (
