1ab4f11bbf4f3727ed7fd3be81901fa94c283bba
1uisf <ldferrer2004@gmail.com>
2025-03-27 20:59:34 -0400

    Added Friends.tsx page to frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/login.tsx';
-import Home from './pages/Home.tsx';
-import SignUp from './pages/signup.tsx'; 
-import Decision from './pages/decision.tsx';
-import Account from './pages/Account.tsx';
-import About from './pages/About.tsx';
-import MainLayout from './components/MainLayout.tsx';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/login.tsx";
+import Home from "./pages/Home.tsx";
+import SignUp from "./pages/signup.tsx";
+import Decision from "./pages/decision.tsx";
+import Account from "./pages/Account.tsx";
+import About from "./pages/About.tsx";
+import Friends from "./pages/Friends.tsx";
+import MainLayout from "./components/MainLayout.tsx";
 
 const App: React.FC = () => {
   return (
@@ -19,6 +20,7 @@ const App: React.FC = () => {
           <Route path="/home" element={<Home />} />
           <Route path="/account" element={<Account />} />
           <Route path="/about" element={<About />} />
+          <Route path="/friends" element={<Friends />} />
         </Route>
       </Routes>
     </Router>
